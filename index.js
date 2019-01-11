21955ac58ebfdf10d4edd95de3f5569ef371798a
Matt Richmond <matt@bigdawggi.com>
2019-01-10 22:07:05 -0700

    updating index.html and index.js to do cool things

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var fs = require("fs");
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var stream = fs.createWriteStream("cats.txt", {flags: "a"});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on("connection", function(socket) {
+    socket.on("chat message", function(msg) {
+        stream.write(msg + "\n");
+        io.emit("chat message", msg);
+    });
+
+    fs.readFile("cats.txt", function(err, fileContents) {
+        socket.emit("chat message", fileContents.toString());
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+    console.log("listening on *:" + port);
 });
